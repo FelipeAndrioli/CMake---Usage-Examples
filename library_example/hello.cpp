a4fcca4697c38cbdeb310a6a575117a2cddb34d9
Felipe Andrioli <felipeandrioli56@hotmail.com>
2021-12-06 11:53:10 -0300

    Added create and add library to cmake example

---
+++ library_example/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.hpp"
+
+#include <iostream>
+
+void hello::hello_there() {
+    std::cout << "Hello there!" << std::endl;
+}
---
+++ library_example/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "hello.hpp"
+
+int main() {
+
+    hello::hello_there();
+
+    return 0;
+}
